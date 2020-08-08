986d69ccd9439fed85c1d10030b8fb9cb20d1a77
Milroy E. Fraser <milroy@outlook.com>
2020-08-09 01:46:56 +0200

    bug fix

--- resources/views/checkout-form.blade.php
+++ resources/views/checkout-form.blade.php
@@ -3,7 +3,7 @@
     <input type="hidden" name="return_url" value="{{ route('pay-here.payment-success') }}">
     <input type="hidden" name="cancel_url" value="{{ route('pay-here.payment-canceled') }}">
     <input type="hidden" name="notify_url" value="{{ route('pay-here.notify') }}">
-    <input type="hidden" name="order_id" value="{{ $payment->getRouteKey() }}">
+    <input type="hidden" name="order_id" value="{{ $payable->getRouteKey() }}">
     <input type="hidden" name="amount" value="{{ $payable->amount }}">
     <input type="hidden" name="currency" value="{{ $payable->currency }}">
     <input type="hidden" name="hash" value="{{ $payable->hash }}">
